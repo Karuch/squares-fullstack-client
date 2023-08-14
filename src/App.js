3b56fc0ca80e79f4a86459c8a3b8180022aab5b2
tal <talk474747@gmail.com>
2023-08-14 15:30:22 +0300

    not sure tough

--- src/App.js
+++ src/App.js
@@ -3,27 +3,12 @@ import { Squares, IncrementButton, ReductionButton } from "./Squares";
 import './components.css'
 
 function App() {
-  let count = 40; // 193 should be max otherwise out of div + should be determine in PSQL
-  const request = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/squares');
-      const data = await response.json();
-      count = data[0].squares_amount;
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
-  };
-  request()
-  console.log(count, "after count"); //can't because the function which change count is async and change after count being rendered so it stays 0
-  const squares_amount = Array.from({ length: count }, (_, index) => (
-    <div key={index}>Element {index + 1}</div>
-  ));
   return (
     <div className="App">
         <div className="parent">
           <div className="div1"> 
             <IncrementButton/>
-            {squares_amount.map((index) => <Squares/>)}
+              <Squares/>
             <ReductionButton/>
           </div>
           <div className="div2"> </div>
--- src/Squares.js
+++ src/Squares.js
@@ -1,9 +1,44 @@
+let count = 40; // 193 should be max otherwise out of div + should be determine in PSQL
+const request = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/squares');
+    const data = await response.json();
+    count = data[0].squares_amount;
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+};
+request()
+console.log(count, "after count"); //can't because the function which change count is async and change after count being rendered so it stays 0
+
+
+
+
+
 function Squares() {
+    const squares_amount = Array.from({ length: count }, (_, index) => (
+      <div key={index}>Element {index + 1}</div>
+    ));
+  
     let color = "green";
     return (
-        <div className="square" style={{backgroundColor: color}}></div>
+      <>
+        {squares_amount.map((index) => <div className="square" style={{backgroundColor: color}}></div>)}
+      </>
     );
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 function IncrementButton(){
   const requestOptions = {
